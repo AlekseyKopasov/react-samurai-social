9d5027a8c2ac0ebf815d46f597a45b19e9e38bd3
Aleksey Kopasov <alesey.k1@gmail.com>
2021-09-15 00:21:10 +0300

    initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.logodesign.net/logo/abstract-cuboid-building-4519ld.png" alt=""/>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://pix10.agoda.net/hotelImages/301716/-1/fe9724d8fb4da3dd4590353bd771a276.jpg?s=1024x768" alt=""/>
+      </div>
+
+      <div>
+        ava + descr
+      </div>
+
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+      </div>
+
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
